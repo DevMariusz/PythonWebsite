5471cb0d3a78c9f41dabdf47a0bb0eaf255e825b
devmariusz <devmariusz.h@gmail.com>
2019-01-13 12:14:43 -0600

    Added user login

--- Flask Website/myproject/__init__.py
+++ Flask Website/myproject/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='84d8dfa6913b747a276174c363653cf8'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
 
 from myproject import routes
